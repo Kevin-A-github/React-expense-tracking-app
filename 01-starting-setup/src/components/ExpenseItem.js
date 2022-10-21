6bfcb3c4e3345cca4aa31694aa12a82effa5313d
Kevin <kabreu11385@gmail.COM>
2022-10-21 17:49:06 -0400

    added the expenses on to their own components

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -5,6 +5,7 @@ import ExpenseDate from './ExpenseDate';
 const ExpenseItem = props => {
   return (
     <div className="expense-item">
+      {/* date={props.date} date is the "variable" we use to pass the props into ExpenseDate.*/}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
